6db5c30fdee16361c246859a60466e1125c8915b
alaminevan <vishall.jha73@gmail.com>
2024-12-24 22:41:45 +0600

    function and objects

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-function caclAge(birthyear) {
-  let age = 2024 - birthyear;
-  console.log(age);
+// function caclAge(birthyear) {
+//   let age = 2024 - birthyear;
+//   console.log(age);
+// }
+// caclAge(1985);
+//
+// function sum() {
+//   console.log("hello from js");
+// }
+// console.log(sum());
+// function appleOrange() {
+//   console.log("amak taka dao");
+// }
+// console.log(appleOrange());
+// let age = 20;
+// console.log("im 20years old");
+// console.log(`i am ${age} years old`);
+// let name = "Alamin";
+// let age = 20;
+// console.log(`im ${name} and, i am ${age} years old`);
+function added(num1,num2){
+    const total = num 1+ num 2;
+    return total
+
 }
-caclAge(1985);
+const sum = added (15,43);
+console.log(sum);
+
+// function appleOrange(apples, oranges, bliss) {
+//   //   console.log(apples, oranges);
+//   const juice = `juice with ${apples} , make with ${oranges} also make ${bliss}`;
+//   return juice;
+// }
+// const juice = appleOrange(1, 2, 1);
+// console.log(juice);
